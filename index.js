c571a581f543c62ba71039b8c19187b6ebe279c9
brainbicycle <brian.beckerle@artsymail.com>
2020-01-21 17:18:01 -0500

    Configure jest with dotenv

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config
 const initDataDogTracer = require('./src/tracer')
 
 const {
---
+++ test.config.js
@@ -0,0 +1,3 @@
+require('dotenv').config({
+  path: require('path').join(process.cwd(), '.env.test'),
+})
