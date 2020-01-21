bb8e251c3474f8a807ab641efcd6550b47bb1892
brainbicycle <brian.beckerle@artsymail.com>
2020-01-21 15:18:01 -0500

    Explicitly use dns trace plugin, rollup other services
    Explicitly use dns trace plugin, rollup other services
    
    Updating tracer tests, disable debug mode for tracer

--- src/tracer.js
+++ src/tracer.js
@@ -4,11 +4,7 @@ module.exports = function init() {
   const ddServicePrefix = 'volley'
 
   tracer.init({
-    // Setting the service name to  `.undetected` so that we detect services that
-    // are running as part of the application, but aren't explicitly configured
-    // with `tracer.use`. We want to to explicitly configure services so that we
-    // can enforce our `.`-deliminited Service naming convention.
-    service: `${ddServicePrefix}.undetected`,
+    service: `${ddServicePrefix}`,
     hostname: process.env.DATADOG_AGENT_HOSTNAME,
   })
 
@@ -18,5 +14,9 @@ module.exports = function init() {
 
   tracer.use('http', {
     service: `${ddServicePrefix}.http`,
+  })
+
+  tracer.use('dns', {
+    service: `${ddServicePrefix}.dns`,
   })
 }
